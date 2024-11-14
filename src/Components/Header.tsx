bb62b09815ebfe49cff9b4d8543e8316be3ea166
Thomas Nguyen <tomxnguy@gmail.com>
2024-11-13 18:37:55 -0800

    Finished front page

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -3,22 +3,27 @@ import { Link, Outlet } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div className="flex w-full bg-blue-300 justify-between">
-      <div className="flex py-2 mx-2">
-        <Link to="/">
-          <button className="items-center rounded-md bg-green-100 px-2">
-            Sets
-          </button>
-        </Link>
-      </div>
-      <div className="w-full flex justify-center items-center ">
-        <h1>Spell Craft</h1>
-      </div>
-      <Link to="/deck">
-        <div className="items-center pt-1 px-4">
-          <img className="" src={deckview} alt="deckview" />
+    <div>
+      <div className="flex w-full bg-blue-300 justify-between">
+        <div className="flex py-2 mx-2">
+          <Link to="/">
+            <button className="items-center rounded-md bg-green-100 px-2">
+              Sets
+            </button>
+          </Link>
         </div>
-      </Link>
+        <div className="w-full flex justify-center items-center ">
+          <Link to="/">
+            <h1>Spell Craft</h1>
+          </Link>
+        </div>
+        <div className="items-center pt-2 px-4">
+          <Link to="/deck">
+            <img className="" src={deckview} alt="deckview" />
+          </Link>
+        </div>
+      </div>
+      <Outlet />
     </div>
   );
 }
--- src/api.ts
+++ src/api.ts
@@ -1,6 +1,5 @@
 export async function readSets() {
   const response = await fetch("https://api.magicthegathering.io/v1/sets");
   const sets = await response.json();
-  console.log(sets);
   return sets;
 }
